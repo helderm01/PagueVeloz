d12de58a334b3406e8ed4065e589bee4b15e8666
Helder Souza <helderm01@gmail.com>
2020-02-13 21:32:53 -0300

    commit parcial

---
+++ Pgvz.Dominio/PessoaTipo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pgvz.Dominio
+{
+    public struct PessoaTipo
+    {
+        private Cpf cpf;
+        private Cnpj cnpj;
+
+
+    }
+}
