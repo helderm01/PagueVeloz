60be367cce88419fc338f0ece32e780a1beb2fcc
Helder Souza <helderm01@gmail.com>
2020-02-13 21:21:12 -0300

    commit parcial

---
+++ Pgvz.Dominio/Pgvz.Dominio/Empresa.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pgvz.Dominio
+{
+    public class Empresa
+    {
+        private string nome;
+        private Cnpj cnpj;
+        private UnidadeFederacaoSigla uf;
+
+        public string Nome { get => nome; }
+        public Cnpj Cnpj { get => cnpj; }
+        public UnidadeFederacaoSigla Uf { get => uf; }
+
+        public Empresa(string nome, string cnpj, UnidadeFederacaoSigla uf)
+        {
+            AtribuirNome(nome);
+            AtribuirCNPJ(cnpj);
+            AtribuirUF(uf);
+        }
+
+        public EntidadeValida AtribuirNome(string nome)
+        {
+            nome = nome.Trim();
+            if (string.IsNullOrEmpty(nome))
+                return new EntidadeValida(false, "Nome é obrigatório!");
+
+            if (nome.Length < 5)
+                return new EntidadeValida(false, "Nome deve conter no mínimo 5 caracteres!");
+
+            if (nome.Length > 24)
+                return new EntidadeValida(false, "Nome deve conter no máximo 25 caracteres!");
+
+            this.nome = nome;
+            return new EntidadeValida(true, string.Empty);
+        }
+
+        public EntidadeValida AtribuirCNPJ(string cnpj)
+        {
+            var cnpjValido = new Cnpj(cnpj);
+            if (!cnpjValido.EhValido)
+                return new EntidadeValida(false, "CNPJ inválido!");
+
+            this.cnpj = cnpjValido;
+            return new EntidadeValida(true, string.Empty);
+        }
+
+        public EntidadeValida AtribuirUF(UnidadeFederacaoSigla uf)
+        {
+            this.uf = uf;
+            return new EntidadeValida(false, string.Empty);
+        }
+    }
+}
---
+++ Pgvz.Dominio/Pgvz.Dominio/EntidadeValida.cs
@@ -0,0 +1,14 @@
+﻿namespace Pgvz.Dominio
+{
+    public struct EntidadeValida
+    {
+        public bool Valida { get; }
+        public string Mensagem { get; }
+
+        public EntidadeValida(bool valida, string mensagem)
+        {
+            Valida = valida;
+            Mensagem = mensagem;
+        }
+    }
+}
